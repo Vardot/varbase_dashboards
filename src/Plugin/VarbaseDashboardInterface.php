b8f9cef134b19da4ec8158f8e58e4d176af735d2
Noor Ghunaim <58352-n.ghunaim@users.noreply.drupalcode.org>
2025-06-24 14:20:33 +0000

    Issue #3526556 by n.ghunaim: Switch From Dashboards to Dashboard Module

---
+++ src/Plugin/VarbaseDashboardInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\varbase_dashboards\Plugin;
+
+use Drupal\Component\Plugin\PluginInspectionInterface;
+
+/**
+ * Defines an interface for Varbase Dashboard plugins.
+ */
+interface VarbaseDashboardInterface extends PluginInspectionInterface {
+
+}
